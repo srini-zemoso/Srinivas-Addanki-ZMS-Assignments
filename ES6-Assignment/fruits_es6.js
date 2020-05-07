165583b4e10636120813a5eaf59e2f9ab1c81d1a
srini-zemoso <64887358+srini-zemoso@users.noreply.github.com>
2020-05-07 11:22:59 +0530

    Upating ES6 Assignment
    Modified changes to make the code work

--- ES6-Assignment/fruits_es6.js
+++ ES6-Assignment/fruits_es6.js
@@ -3,9 +3,10 @@ class Fruit{
         this.name = name;
         this.quant = quant;
         this.unit = unit;
-        let calcTP = (quant,unit) => {(quant*unit)};
+        }
+        
     }
-};
+
 
 Strawberry = new Fruit('Strawberry',15,10);
 Pineapple = new Fruit('Pineapple',10,40);
@@ -13,20 +14,17 @@ Apple = new Fruit('Apple',6,20);
 Mango = new Fruit('Mango',12,50);
 Pear = new Fruit('Pear',10,80);
 
-let fruits = [Strawberry.name,Pineapple.name,Apple.name,Mango.name,Pear.name];
-let quantity = [Strawberry.qaunt,Pineapple.qaunt,Apple.quant,Mango.quant,Pear.quant];
-let unitprice = [Strawberry.unit,Pineapple.unit,Apple.unit,Mango.unit,Pear.unit];
+const fruits = [Strawberry,Pineapple,Apple,Mango,Pear];
 
-
-let gt250 = [];
-for (let i=0;i<fruits.length;i++){
-    
-    if(fruits[i].calcTP>=250){
-        gt250.push(fruits[i]);
+let calcPrice = () => {
+    for(let fruit of fruits){
+        const price = fruit.quant*fruit.unit;
+        if(price>250){
+            console.log(fruit.name);
+        }
     }
-    
 }
 
-console.log(gt250);
+calcPrice(fruits);
 
 
